3f7f3039dc7a0e0a28de0946e059d4e30b3aedee
jmlm <jmlm74@gmail.com>
2022-08-29 15:56:44 +0200

    Virtual-Select (Detail) + listbox(Info) -> OK

--- app/backups/static/js/script.js
+++ app/backups/static/js/script.js
@@ -10,6 +10,7 @@ window.onload = function() {
         let url_sans_repo = url.substring(0,posit-1);
         posit = url_sans_repo.lastIndexOf('/') + 1;
         let repo_id = url_sans_repo.substring(posit);
+        let is_archive = url.indexOf('archive_info');
 
         
         let target_detail = document.querySelector('#archive_detail_col1');
@@ -55,52 +56,52 @@ window.onload = function() {
                 response.json().then(function(data){
                     let data2 = data[0]['data'];
                     if (data2.length === 400){ alert ("Attention - Résultat tronqué à 400 lignes !")} ;
-                    // mise des lifgnes dans une  textarea
-                    /*
-                    let my_textarea = document.createElement('textarea');
-                    for (const ligne of data2) {
-                        my_textarea.value += ligne;
-                        my_textarea.value += '\n';
-                    }
-                    my_textarea.rows = 15
-                    my_textarea.cols = 100
-                    my_textarea.classList.add("ms-5", "mt-3")
-                    target_detail.appendChild(my_textarea);
-                    */
                     // suppression du bouton et de l'anim ==> Traitement terminé !
                     button_loader_div.parentElement.removeChild(button_loader_div);
-                    //
-                    // ICI Select
-                    // 
-                    let my_div = document.createElement('div');
-                    my_div.id = "my_select";
-                    my_div.classList.add("ms-5")
-                    my_div.classList.add("mt-3")
-                    target_detail.appendChild(my_div);
-                    let options_array = [];
-                    let cprt = 0;
-                    for (const ligne of data2){
-                        const option = { label: ligne, value: cprt ,customData: 'data-tooltip=ligne'};
-                        cprt++;
-                        options_array.push(option);
-                    };
-                    // la mise en forme sexy
-                    VirtualSelect.init({
-                        ele: '#my_select',
-                        options: options_array,
-                        multiple: true,
-                        tooltipMaxWidth: '800px',
-                        dropboxWidth: '500px',
-                        keepAlwaysOpen: true
-                    });
-                    let my_button = document.createElement('button');
-                    my_button.innerHTML = "Restore";
-                    my_button.classList.add("btn");
-                    my_button.classList.add("btn-warning")
-                    my_button.classList.add("align-middle")
+                    
+                    if (is_archive != -1){
+                        // mise des lignes dans une  textarea <=> Archive Info
+                        let my_textarea = document.createElement('textarea');
+                        for (const ligne of data2) {
+                            my_textarea.value += ligne;
+                            my_textarea.value += '\n';
+                        }
+                        my_textarea.rows = 15
+                        my_textarea.cols = 100
+                        my_textarea.classList.add("ms-5", "mt-3")
+                        target_detail.appendChild(my_textarea);
+                    } else {
+                        // Select pour restore <=> Archive detail                 
+                        let my_div = document.createElement('div');
+                        my_div.id = "my_select";
+                        my_div.classList.add("ms-5")
+                        my_div.classList.add("mt-3")
+                        target_detail.appendChild(my_div);
+                        let options_array = [];
+                        let cprt = 0;
+                        for (const ligne of data2){
+                            const option = { label: ligne, value: cprt ,customData: 'data-tooltip=ligne'};
+                            cprt++;
+                            options_array.push(option);
+                        };
+                        // la mise en forme sexy
+                        VirtualSelect.init({
+                            ele: '#my_select',
+                            options: options_array,
+                            multiple: true,
+                            tooltipMaxWidth: '800px',
+                            dropboxWidth: '500px',
+                            keepAlwaysOpen: true
+                        });
+                        let my_button = document.createElement('button');
+                        my_button.innerHTML = "Restore";
+                        my_button.classList.add("btn");
+                        my_button.classList.add("btn-warning")
+                        my_button.classList.add("align-middle")
 
-                    target_detail2.appendChild(my_button);
-                })
+                        target_detail2.appendChild(my_button);
+                    }
+                });
             })
             .catch(error => {
                 console.log("Erreur" + error);
